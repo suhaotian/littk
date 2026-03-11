8122e6f8b8ef86075a18c6631e6b742a3965ef2d
suhaotian <h4av.one@gmail.com>
2026-03-12 01:43:33 +1100

    Fix demos public path

--- demos/build.ts
+++ demos/build.ts
@@ -144,7 +144,7 @@ const result = await Bun.build({
     "process.env.NODE_ENV": JSON.stringify("production"),
   },
   ...cliConfig,
-  publicPath: '/littk/',
+  publicPath: '/littkk/',
 });
 
 const end = performance.now();
